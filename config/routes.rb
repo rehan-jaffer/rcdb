7f0488834f446582ec0072af3d79585085ee31b2
Ray <ray@thelondonvandal.com>
2015-09-11 10:24:05 +0000

    added cannabinoids route, and admin backend namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :benzodiazepines
+  resources :cannabinoids
   resources :home, :only => [:index]
   resources :drugs
   root to: "home#index"
+
+  namespace :admin do
+    resources :benzodiazepines
+  end
 
 end
