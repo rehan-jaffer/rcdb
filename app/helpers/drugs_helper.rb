4a0093b9d8005857b608c6ea5eb1c9e66d1a716c
Ray <ray@thelondonvandal.com>
2015-09-11 10:25:49 +0000

    added google trends graph helper

--- app/helpers/drugs_helper.rb
+++ app/helpers/drugs_helper.rb
@@ -1,2 +1,7 @@
 module DrugsHelper
+
+  def display_google_trends_graph(drug_name)
+    return "<script type = 'text/javascript' src = '//www.google.com/trends/embed.js?hl=en-US&q=buy+#{drug_name}&cmpt=q&tz=Etc/GMT&tz=Etc/GMT&content=1&cid=TIMESERIES_GRAPH_0&export=5&w=500&h=330'></script>".html_safe
+  end
+
 end
