1c6c25d25dd89748aff3a109d40ea657798b5ecc
Ray <ray@thelondonvandal.com>
2015-09-11 10:25:28 +0000

    added uniqueness requisite for drug primary name, and relationships to research papers and reports

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,6 +1,11 @@
 class Drug < ActiveRecord::Base
   actable
   has_many :resources
+  has_many :papers
+  has_many :reports
+
+  validates_presence_of :primary_name
+  validates_uniqueness_of :primary_name
 
   def to_param
     primary_name
