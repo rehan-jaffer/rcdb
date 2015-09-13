4f925639951bdbdc6a829a62fc8d9e69ee3ff53c
Ray <ray@thelondonvandal.com>
2015-09-13 21:56:09 +0000

    added fresh specs from generators

---
+++ spec/factories/edits.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :edit do
+    
+  end
+
+end
--- spec/models/drug_spec.rb
+++ spec/models/drug_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Drug, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "#validation" do
+
+  end
+
+  context "#functionality" do
+
+  end
+
 end
