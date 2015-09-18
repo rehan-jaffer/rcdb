9402fffe365327ba94bcf76f5d54b6f1f80cbb60
Ray <ray@thelondonvandal.com>
2015-09-18 11:31:41 +0000

    add spaces between comma separated 'names'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@ module ApplicationHelper
 
   def display_other_names(names)
     unless names.nil?
-      return names.map { |name| name }.join(",")
+      return names.map { |name| name }.join(", ")
     end
   end
 
   def display_trade_names(names)
     unless names.nil?
-      return names.map { |name| name }.join(",")
+      return names.map { |name| name }.join(", ")
     end
   end
 
