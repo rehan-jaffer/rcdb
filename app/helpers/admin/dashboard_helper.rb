d851ae9a9ee5716e4d4a96e9a96f3dfed6f9c30f
Ray <ray@thelondonvandal.com>
2015-09-23 09:55:24 +0000

    refactored affinity input hstore into helper

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -1,2 +1,14 @@
 module Admin::DashboardHelper
+
+  def affinity_inputs_helper(form_object, receptor_list)
+   form = ''
+   form_object.simple_fields_for :affinity do |receptor_inputs|
+     receptor_list.each do |receptor|
+       form << content_tag(:div,
+         receptor_inputs.input(receptor, input_html: {:g1 => "g1"}, size: 4),
+       class: "col-lg-8 col-md-8")
+     end
+   end
+   form.html_safe
+  end
 end
