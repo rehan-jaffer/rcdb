eb2528d052ed189c0be4b9496f857192d6bec512
Ray <ray@thelondonvandal.com>
2015-09-23 09:57:25 +0000

    added subtype method to base class

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -13,4 +13,12 @@ class Drug < ActiveRecord::Base
     primary_name
   end
 
+  private
+
+    def affinity_accessors(receptors)
+      receptors.each do |subtype|
+        store_accessor :affinity, subtype
+      end
+    end
+
 end
