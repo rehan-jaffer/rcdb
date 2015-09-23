6654cc206d638e23163eebb48aa56fc804fcc8ed
Ray <ray@thelondonvandal.com>
2015-09-23 09:55:53 +0000

    refactored subtype affinity hstore into base drug model

--- app/models/benzodiazepine.rb
+++ app/models/benzodiazepine.rb
@@ -3,8 +3,10 @@ class Benzodiazepine < ActiveRecord::Base
   acts_as :drug
 
   # refactor this to use an array of hstore keys
-  [:α1, :α2, :α3, :α4, :α5, :α6, :β1, :β2, :β3, :γ1, :γ2, :γ3].each do |subtype|
-    store_accessor :affinity, subtype
+  def initialize
+    self.class_eval do
+      affinity_accessors([:α1, :α2, :α3, :α4, :α5, :α6, :β1, :β2, :β3, :γ1, :γ2, :γ3])
+    end
   end
 
   def to_valium
