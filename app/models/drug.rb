a6037377dec8cb89ba2228af14bbfbe731fedba6
Ray <ray@thelondonvandal.com>
2015-09-24 07:43:24 +0000

    use a mixin rather than inheritance

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -13,12 +13,4 @@ class Drug < ActiveRecord::Base
     primary_name
   end
 
-  private
-
-    def affinity_accessors(receptors)
-      receptors.each do |subtype|
-        store_accessor :affinity, subtype
-      end
-    end
-
 end
