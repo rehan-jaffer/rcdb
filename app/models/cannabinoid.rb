df850e87f445aed233c62ac84f907e11dc0189da
Ray <ray@thelondonvandal.com>
2015-09-24 18:08:41 +0000

    added correct subtypes for cannabinoid model

--- app/models/cannabinoid.rb
+++ app/models/cannabinoid.rb
@@ -1,9 +1,19 @@
 class Cannabinoid < ActiveRecord::Base
 
+  extend Drug::DrugProperties
+
   acts_as :drug
 
   def to_param
     primary_name
+  end
+
+  def self.receptor_list
+      [:cb1, :cb2]
+  end
+
+  self.class_eval do
+    affinity_accessors(receptor_list)
   end
 
 end
