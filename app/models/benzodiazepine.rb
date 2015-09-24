14d28e33f85cae65b7fc447cbcd42f3eab50b717
Ray <ray@thelondonvandal.com>
2015-09-24 07:44:08 +0000

    extend the drug properties module for dynamically adding receptor subtypes

--- app/models/benzodiazepine.rb
+++ app/models/benzodiazepine.rb
@@ -1,14 +1,10 @@
 class Benzodiazepine < ActiveRecord::Base
 
+  extend Drug::DrugProperties
+
   acts_as :drug
 
   # refactor this to use an array of hstore keys
-  def initialize
-    self.class_eval do
-      affinity_accessors([:α1, :α2, :α3, :α4, :α5, :α6, :β1, :β2, :β3, :γ1, :γ2, :γ3])
-    end
-  end
-
   def to_valium
     valium_equiv || "unknown"
   end
@@ -16,5 +12,13 @@ class Benzodiazepine < ActiveRecord::Base
   def to_param
     primary_name
   end
- 
+
+  def self.receptor_list
+      [:α1, :α2, :α3, :α4, :α5, :α6, :β1, :β2, :β3, :γ1, :γ2, :γ3]
+  end
+
+  self.class_eval do
+    affinity_accessors(receptor_list)
+  end
+
 end
