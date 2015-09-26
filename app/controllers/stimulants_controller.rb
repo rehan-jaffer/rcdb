01ad2169b6112c38079bb8afff2245aa8e1f096e
Ray <ray@thelondonvandal.com>
2015-09-26 09:50:58 +0000

    added basic behaviour to stimulants controller

--- app/controllers/stimulants_controller.rb
+++ app/controllers/stimulants_controller.rb
@@ -1,2 +1,15 @@
 class StimulantsController < ApplicationController
+
+  def index
+   @stimulants = Stimulant.all
+  end
+
+  def show
+   @stimulant = Stimulant.find_by_primary_name(stimulant_params[:primary_name])
+  end
+
+  def stimulant_params
+    params.require(:stimulant).permit([:primary_name])
+  end
+
 end
