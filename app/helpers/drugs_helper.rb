ab6700c4f260efb608aee9ee2e7d88c16163fc43
Ray <ray@thelondonvandal.com>
2015-09-27 05:08:29 +0000

    added select_drug_path method for switching drug path based on drug type

--- app/helpers/drugs_helper.rb
+++ app/helpers/drugs_helper.rb
@@ -4,4 +4,11 @@ module DrugsHelper
     return "<script type = 'text/javascript' src = '//www.google.com/trends/embed.js?hl=en-US&q=buy+#{drug_name}&cmpt=q&tz=Etc/GMT&tz=Etc/GMT&content=1&cid=TIMESERIES_GRAPH_0&export=5&w=500&h=330'></script>".html_safe
   end
 
+  def select_drug_path(drug)
+  
+    drug_type = drug.class
+    return send("#{drug_type.to_s.downcase}_path", drug.primary_name)
+
+  end
+
 end
