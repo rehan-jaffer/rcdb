6fd3b6489d2fef9ffb462e07663508a4635c7ddb
Ray <ray@thelondonvandal.com>
2015-10-01 11:42:57 +0000

    switched to drug layout and added title

--- app/controllers/benzodiazepines_controller.rb
+++ app/controllers/benzodiazepines_controller.rb
@@ -5,8 +5,11 @@ class BenzodiazepinesController < ApplicationController
   end
 
   def show
-    @benzodiazepine = Benzodiazepine.where(:primary_name => params[:id]).last
+    @drug = Benzodiazepine.where(:primary_name => params[:id]).last
+    @title = "Benzodiazepines / #{@drug.primary_name}"
     @report = Report.new
+    render 'application/drug'
+    return
   end
 
 end
