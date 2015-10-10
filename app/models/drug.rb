6f38834ca8c26fde286c3c7de645e6ce1baf08c1
Ray <ray@thelondonvandal.com>
2015-10-10 15:43:09 +0000

    added hacky method for removing empty entries from the array

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -10,6 +10,15 @@ class Drug < ActiveRecord::Base
   validates_presence_of :primary_name
   validates_uniqueness_of :primary_name
 
+  before_save :eliminate_empties
+
+  # this is a hacky fix
+  def eliminate_empties
+    ["classes", "trade_names", "other_names"].each do |field|
+      send(field.to_sym).reject! { |item| item.empty? }
+    end
+  end
+
   def to_param
     primary_name
   end
