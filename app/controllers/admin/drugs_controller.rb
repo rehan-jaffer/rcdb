80a9dd5ad96967f0aa732571e12f02ffce759410
Ray <ray@thelondonvandal.com>
2015-10-11 16:47:24 +0000

    added drug controller to add base controller to build drug controllers from

---
+++ app/controllers/admin/drugs_controller.rb
@@ -0,0 +1,7 @@
+class Admin::DrugsController < Admin::AdminController
+
+  def drug_attributes
+    [:molecule_image, :molecule_image_source, :id, :primary_name, :description, :half_life, :full_name, :paper_feed, :onset, trade_names: [], other_names: [], classes: [], solubility: [], side_effects: [], references: []]
+  end
+
+end
