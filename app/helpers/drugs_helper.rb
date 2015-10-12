c8e798f89fbb3a110c2089db252940877f0f3a17
Ray <ray@thelondonvandal.com>
2015-10-12 11:27:39 +0000

    added helper for stylized panel

--- app/helpers/drugs_helper.rb
+++ app/helpers/drugs_helper.rb
@@ -1,5 +1,9 @@
 module DrugsHelper
 
+  def styled_panel_helper(title, body)
+    render :partial => 'panel', locals: {title: title, body: body}
+  end
+
   def display_google_trends_graph(drug_name)
     return "<script type = 'text/javascript' src = '//www.google.com/trends/embed.js?hl=en-US&q=buy+#{drug_name}&cmpt=q&tz=Etc/GMT&tz=Etc/GMT&content=1&cid=TIMESERIES_GRAPH_0&export=5&w=500&h=330'></script>".html_safe
   end
