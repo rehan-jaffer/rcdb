14f9be66edf1197d82e842b6e49a9c0e1dd05cdb
Ray <ray@thelondonvandal.com>
2015-10-17 16:34:02 +0000

    added citation parser for description text

---
+++ app/models/drug/citation_parser.rb
@@ -0,0 +1,24 @@
+module Drug::CitationParser
+
+  def parse!
+    citations = []
+
+    self.description.scan(/\[(http[^\[\]]*)\]/) do |match|
+      citations << match
+    end
+
+    n = 0
+
+    citations.each do |citation|
+
+      n += 1
+      link = citation[0]
+      self.description = self.description.gsub("\[#{link}\]", "<a href='#citation-#{n}'>[#{n.to_s}]</a>")
+      self.description += "\r\n<li>#{n.to_s}. <a href='#{link}' id='citation-#{n}'>#{link}</a></li>\r\n"
+
+    end
+
+    self.description
+  end
+
+end
