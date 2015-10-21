ad90f8f751579c0b794f3c4fee2f293a28323f98
Ray <ray@thelondonvandal.com>
2015-10-21 09:47:09 +0000

    fleshed out contents of metabolites controller

--- app/controllers/admin/metabolites_controller.rb
+++ app/controllers/admin/metabolites_controller.rb
@@ -1,2 +1,31 @@
 class Admin::MetabolitesController < ApplicationController
+
+  def new
+    @metabolite = Metabolite.new
+  end
+
+  def edit
+    @metabolite = Metabolite.find(params[:id])
+  end
+
+  def create
+    @metabolite = Metabolite.new(metabolite_params)
+    if @metabolite.save
+      redirect_to admin_metabolites_path
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def index
+    @metabolites = Metabolite.all
+  end
+
+  def metabolite_params
+    params.require(:metabolite).permit(:drug_id, :name, :active)
+  end
+
 end
