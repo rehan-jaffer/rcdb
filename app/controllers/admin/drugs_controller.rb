f0baecef783e302537db09c031154dd6bf74823b
Ray <ray@thelondonvandal.com>
2015-10-24 08:11:38 +0000

    added drugs controller strong params

--- app/controllers/admin/drugs_controller.rb
+++ app/controllers/admin/drugs_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DrugsController < Admin::AdminController
 
   def drug_attributes
-    [:molecule_image, :molecule_image_source, :id, :primary_name, :description, :half_life, :full_name, :paper_feed, :onset, trade_names: [], other_names: [], classes: [], solubility: [], side_effects: [], references: []]
+    [:harm_rating, :addiction_rating, :molecule_image, :molecule_image_source, :id, :primary_name, :description, :half_life, :full_name, :paper_feed, :onset, trade_names: [], other_names: [], classes: [], solubility: [], side_effects: [], references: []]
   end
 
 end
