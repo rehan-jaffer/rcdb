5c691f330f03e3d77c66352ab6cd077ec951f829
Ray <ray@thelondonvandal.com>
2015-10-25 14:59:48 +0000

    added basic behaviour for psychedelic controller

--- app/controllers/psychedelics_controller.rb
+++ app/controllers/psychedelics_controller.rb
@@ -1,2 +1,15 @@
-class PsychedelicsController < ApplicationController
+class PsychedelicsController < DrugsController
+
+  def index
+    @psychedelics = Psychedelic.all.to_a
+  end
+
+  def show
+    @drug = Psychedelic.where(:primary_name => params[:id]).last
+    @title = "Psychedelics / #{@drug.primary_name}"
+    @report = Report.new
+    render 'application/drug'
+    return
+  end
+
 end
