721368381a155c03ffcc5d069ded2bde014f5a07
Ray <ray@thelondonvandal.com>
2015-10-25 15:00:15 +0000

    refactored citation parser to use Parser library

--- app/models/drug/citation_parser.rb
+++ app/models/drug/citation_parser.rb
@@ -6,22 +6,7 @@ module Drug::CitationParser
       self.description = ""
     end
 
-    citations = []
-
-    self.description.scan(/\[(http[^\[\]]*)\]/) do |match|
-      citations << match
-    end
-
-    n = 0
-
-    citations.each do |citation|
-
-      n += 1
-      link = citation[0]
-      self.description = self.description.gsub("\[#{link}\]", "<a href='#citation-#{n}'>[#{n.to_s}]</a>")
-      self.description += "\r\n<li>#{n.to_s}. <a href='#{link}' id='citation-#{n}'>#{link}</a></li>\r\n"
-
-    end
+    self.description = LinkParser.parse(self.description)
 
     self.description
   end
