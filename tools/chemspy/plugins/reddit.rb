d264cc8cabaace0698fe42c48f1d62719086dd5a
Ray <ray@thelondonvandal.com>
2015-10-28 11:31:46 +0000

    return all threads text along with comment contents

--- tools/chemspy/plugins/reddit.rb
+++ tools/chemspy/plugins/reddit.rb
@@ -1,10 +1,15 @@
 require 'mechanize'
 require 'open-uri'
+require 'digest/md5'
 
 class RedditPlugin
 
   def new
     
+  end
+
+  def hash(title)
+    return Digest::MD5.hexdigest(title)
   end
 
   def reload
@@ -14,28 +19,55 @@ class RedditPlugin
   def refresh
    base_url = "https://api.reddit.com/r/"
    subreddits = ["researchchemicals", "noids", "stims","benzodiazepines"]
+#   subreddits = ["noids"]
 
    results = {}
 
+   data = []
+
    subreddits.each do |subreddit|
+
+     puts subreddit
 
      sleep 1
 
-     open("#{base_url}#{subreddit}/new") do |file|
-       results = JSON.parse(file.read)
+     begin
+       open("#{base_url}#{subreddit}/new") do |file|
+         results = JSON.parse(file.read)
+       end
+     rescue
+       puts "Could not access subreddit"
+       sleep 1
+       retry
      end
 
-     pp results["data"]["children"].map { |result| 
+     data << results["data"]["children"].map { |result| 
+      hash = hash(result["data"]["title"])
+      comments = []
       if result["data"]["num_comments"] > 0
-        open("#{base_url}#{subreddit}/#{result[:id]}") do |comment_file|
-          comments = JSON.parse(comment_file.read)
-          pp comments
+
+        sleep 1
+
+        begin
+          open("#{base_url}#{subreddit}/#{result[:id]}") do |comment_file|
+            comments = JSON.parse(comment_file.read)["data"]["children"]
+          end
+        rescue
+          puts "Could not grab comments.."
+          sleep 1
+          retry
         end
+
       end
-      {text: result[:selftext], comments: comments}
+      {text: result["data"]["selftext"], title: result["data"]["title"], comments: comments.map { |comment|
+        {title: comment["data"]["title"], text: comment["data"]["selftext"], hash: hash(comment["data"]["title"]), source: "reddit"}
+      }, hash: hash, source: "reddit"}
      }
 
+     return data
+
    end
+
 
   end
 
