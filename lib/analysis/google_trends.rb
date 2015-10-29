d485022fba42f10530f3955ebc49db8f52887f74
Ray <ray@thelondonvandal.com>
2015-10-29 08:01:21 +0000

    updated google trends module/class to contain methods for fetching arrays

--- lib/analysis/google_trends.rb
+++ lib/analysis/google_trends.rb
@@ -1,24 +1,55 @@
 require 'open-uri'
 require 'nokogiri'
 require 'execjs'
+require 'pp'
 
 module GoogleTrends
+
+  def self.data_set(drug_name)
+
+    data_set = obtain_data(drug_name)
+
+    numbers = data_set["rows"].map do |entry|
+      entry[3]
+    end
+
+    return numbers
+
+  end
+
+  def self.buy_data_set(drug_name)
+
+    data_set = obtain_data(drug_name, true)
+
+    numbers = data_set["rows"].map do |entry|
+      entry[3]
+    end
+
+    return numbers
+
+  end
 
   def self.obtain_start_date(drug_name)
 
     data_set = obtain_data(drug_name)
 
-    non_empty = data_set["rows"].keep_if do |entry|
+    numbers = data_set["rows"].keep_if do |entry|
       entry[3].to_i > 0
     end
 
-    return non_empty.first[0]["v"]
+#    return numbers
+
+    return numbers.first[0]["v"]
  
   end
 
-  def self.obtain_data(drug_name)
+  def self.obtain_data(drug_name, buy=false)
 
-    html = Nokogiri::HTML(open("http://www.google.com/trends/fetchComponent?hl=en-US&q=#{drug_name}&cmpt=q&tz=Etc/GMT&tz=Etc/GMT&content=1&cid=TIMESERIES_GRAPH_0&export=5&w=500&h=330"))
+    if buy == false
+      html = Nokogiri::HTML(open("http://www.google.com/trends/fetchComponent?hl=en-US&q=#{drug_name}&cmpt=q&tz=Etc/GMT&tz=Etc/GMT&content=1&cid=TIMESERIES_GRAPH_0&export=5&w=500&h=330"))
+    else
+      html = Nokogiri::HTML(open("http://www.google.com/trends/fetchComponent?hl=en-US&q=buy+#{drug_name}&cmpt=q&tz=Etc/GMT&tz=Etc/GMT&content=1&cid=TIMESERIES_GRAPH_0&export=5&w=500&h=330"))
+    end
     js = html.xpath("//script")[4].inner_html
     json_data = js.split("chartData = ")[1].split("; var htmlChart")[0]
     data_set = ExecJS.eval(json_data)
