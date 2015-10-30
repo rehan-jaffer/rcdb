c8c62961cfe1dc549ff241215a3305b690ec566c
Ray <ray@thelondonvandal.com>
2015-10-30 12:24:17 +0000

    added model functionality for dissociative and psychedelic models that extend the base drug path and provide the affinity hstore accessors

--- app/models/dissociative.rb
+++ app/models/dissociative.rb
@@ -1,5 +1,21 @@
 class Dissociative < ActiveRecord::Base
 
+  extend Drug::DrugProperties
+
   acts_as :drug
+
+  def to_param
+    primary_name
+  end
+
+  def self.receptor_list
+      []
+  end
+
+  self.class_eval do
+    affinity_accessors(receptor_list)
+    onset_accessors
+  end
+
 
 end
--- app/models/psychedelic.rb
+++ app/models/psychedelic.rb
@@ -1,5 +1,21 @@
 class Psychedelic < ActiveRecord::Base
 
+  extend Drug::DrugProperties
+
   acts_as :drug
+
+  def to_param
+    primary_name
+  end
+
+  def self.receptor_list
+      ["5HT2a"]
+  end
+
+  self.class_eval do
+    affinity_accessors(receptor_list)
+    onset_accessors
+  end
+
 
 end
