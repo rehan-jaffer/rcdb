ee794f4d7bbc84ddf006f88c8f55d284ce0cd079
Ray <ray@thelondonvandal.com>
2015-10-30 19:15:00 +0000

    added stimulants admin controller, using base drug controller to inherit behaviour

---
+++ app/controllers/admin/stimulants_controller.rb
@@ -0,0 +1,15 @@
+class Admin::StimulantsController < Admin::DrugsController
+
+  def class_type
+    Stimulant
+  end
+
+  def class_name
+    "stimulant"
+  end
+
+  def stimulants_attributes
+    params.require(:stimulant).permit(drug_attributes | [affinity: Stimulant.stored_attributes[:affinity]])
+  end
+
+end
