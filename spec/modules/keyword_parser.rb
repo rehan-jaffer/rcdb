8c00c9efc68f99f200f24e252e6ea667a5fdab25
Ray <ray@thelondonvandal.com>
2015-10-31 12:44:52 +0000

    added spec file for testing functionality of keyword parser module

---
+++ spec/modules/keyword_parser.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe KeywordParser do
+
+  before(:all) do
+    keyword = Keyword.new
+    keyword.keyword = "test"
+    keyword.definition = "test text"
+    keyword.save
+  end
+
+  describe "parser functionality" do
+
+    it "parses the text as required" do
+      text = "this is a test of the parser"
+      new_text = KeywordParser.scan(text)
+      expect(new_text).to eq('this is a <a data-toggle="tooltip" data-placement="top" title="test text">Test</a> of the parser')
+    end
+
+  end
+
+end
