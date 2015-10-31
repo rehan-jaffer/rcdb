e4f6dd9130850ab89939a3c4a93c9aa2b6ee2baa
Ray <ray@thelondonvandal.com>
2015-10-31 12:46:08 +0000

    added keyword parser module, fully functional

---
+++ lib/parsers/keyword_parser.rb
@@ -0,0 +1,21 @@
+module KeywordParser
+
+  def self.scan(text)
+
+    stripped_text = text.gsub(/[^\w\d ]/, "").downcase
+    tokens = stripped_text.split(" ")
+    keywords = Keyword.all
+    new_text = ""
+    keywords.each do |keyword|
+      if tokens.include?(keyword.keyword)
+        new_text = text.gsub(keyword.keyword, tooltip(keyword.keyword, keyword.definition))
+      end
+    end
+    new_text
+  end
+
+  def self.tooltip(word, text)
+    %Q(<a data-toggle="tooltip" data-placement="top" title="#{text.html_safe}">#{word.capitalize}</a>).html_safe
+  end
+
+end
