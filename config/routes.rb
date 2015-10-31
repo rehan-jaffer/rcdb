8ce8a6721177cd5a632ff72e139bd9820d70a6ba
Ray <ray@thelondonvandal.com>
2015-10-31 15:02:37 +0000

    added new routes to admin section

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
     resources :metabolites
     resources :stimulants
     resources :dashboard
+    resources :posts
+    resources :keywords
   end
 
   namespace :moderator do
