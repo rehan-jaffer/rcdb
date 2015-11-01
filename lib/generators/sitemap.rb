fb8b57673a0b0ed40c967250d420db40b3bc30e1
Ray <ray@thelondonvandal.com>
2015-11-01 12:32:22 +0000

    updated sitemap generator

--- lib/generators/sitemap.rb
+++ lib/generators/sitemap.rb
@@ -4,29 +4,11 @@ class Sitemap
 
   end
 
-  def add_entry(entry)
-     output += "<url>\r\n"
-     output += "<loc>#{entry.url}</loc>\r\n"
-     output += "<lastmod>#{entry.lastmod}</lastmod>\r\n"
-#        <changefreq>daily</changefreq>
-#        <priority>0.8</priority>"
-     if entry.image?
-        output += "<image:image>"
-        output += "<image:loc>#{entry.image_url}</image:loc>"
-        output += "<image:caption>#{entry.image_description}</image:caption>"
-        output += "</image:image>"
-      end
-    output += "</url>"
-    output
-  end
-
-  def enclose(contents)
+  def self.enclose(contents)
     return '<?xml version="1.0" encoding="utf-8"?>
      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-           xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">'
-     + contents +
-     '</urlset>'
+           xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' + contents + '</urlset>'
 
   end
 
