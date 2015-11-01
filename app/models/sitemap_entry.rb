f2b4e5889344e1080eb6ffa68a346e77a1baf22f
Ray <ray@thelondonvandal.com>
2015-11-01 12:32:12 +0000

    updated sitemap entry to output html

--- app/models/sitemap_entry.rb
+++ app/models/sitemap_entry.rb
@@ -6,12 +6,12 @@ class SitemapEntry
   def to_html
     output = ""
     output += "<url>\r\n"
-    output += "<loc>#{entry.url}</loc>\r\n"
-    output += "<lastmod>#{entry.lastmod}</lastmod>\r\n"
-    if image?
+    output += "<loc>#{url}</loc>\r\n"
+    output += "<lastmod>#{lastmod}</lastmod>\r\n"
+    unless image.nil?
       output += "<image:image>"
-      output += "<image:loc>#{entry.image_url}</image:loc>"
-      output += "<image:caption>#{entry.image_description}</image:caption>"
+      output += "<image:loc>#{image_url}</image:loc>"
+      output += "<image:caption>#{image_description}</image:caption>"
       output += "</image:image>"
     end
     output += "</url>"
