81914cbd63e997b3452dbdf7e3f400a6954dbec5
Ray <ray@thelondonvandal.com>
2015-11-03 08:25:47 +0000

    test parser functionality in drug model

--- spec/models/drug_spec.rb
+++ spec/models/drug_spec.rb
@@ -7,8 +7,7 @@ RSpec.describe Drug, type: :model do
     @drug = Drug.new
     @drug.primary_name = "Sample Drug"
     @drug.description = "This is a test [http://www.test.com/]"
-    @drug.parse!
-    expect(@drug.to_html).to eq "This is a test <a href='#citation-1'>[1]</a>\r\n<li>1. <a href='http://www.test.com/' id='citation-1'>http://www.test.com/</a></li>\r\n"
+    expect(@drug.to_html).to eq "This is a test <a href='#citation-1'>[1]</a><h4 class='citation-header'>+ Citations</h4>\r\n<ul class='citation-listing'>\r\n\r\n<li>1. <a href='http://www.test.com/' id='citation-1'>http://www.test.com/</a></li>\r\n</ul>\r\n"
 
   end
 
