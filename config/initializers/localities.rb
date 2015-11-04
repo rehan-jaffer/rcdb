47602c14f0b400c9c7089a44a032148151ce01e3
Ray <ray@thelondonvandal.com>
2015-11-04 12:41:40 +0000

    added country loading

--- config/initializers/localities.rb
+++ config/initializers/localities.rb
@@ -1,2 +1,12 @@
-COUNTRY_CODES = []
-FULL_NAMES = {}
+require 'csv'
+require 'pp'
+
+country_data = CSV.read("data/countries.csv")
+
+countries_hash = {}
+
+country_data.map do |entry|
+  countries_hash[entry[10]] = entry[1]
+end
+
+pp countries_hash
