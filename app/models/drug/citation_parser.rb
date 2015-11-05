e4c80577ca081e8bab5447018af119869062da25
Ray <ray@thelondonvandal.com>
2015-11-05 16:28:45 +0000

    removed parse! function, drug model now uses linkparser

--- app/models/drug/citation_parser.rb
+++ app/models/drug/citation_parser.rb
@@ -1,14 +1,3 @@
 module Drug::CitationParser
 
-  def parse!
-
-    if self.description.nil?
-      self.description = ""
-    end
-
-    self.description = LinkParser.parse(self.description)
-
-    self.description
-  end
-
 end
