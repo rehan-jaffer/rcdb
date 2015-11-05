e4554752f4b1970f05336068959433902092d694
Ray <ray@thelondonvandal.com>
2015-11-05 16:23:35 +0000

    check dissociative model can parameterize itself

--- spec/models/dissociative_spec.rb
+++ spec/models/dissociative_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Dissociative, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:all) do
+    dissociative = Dissociative.new
+    dissociative.primary_name = "Ketamine"
+    dissociative.save
+  end
+
+  describe "drug class basic behaviour" do
+
+    it "parameterizes itself appropriately" do
+      dissociative = Dissociative.first
+      expect(dissociative.to_param).to eq("Ketamine")
+    end
+
+  end
+
 end
