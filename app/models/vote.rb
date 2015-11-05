80e36829f148b05729462cbf25144ebb82352020
Ray <ray@thelondonvandal.com>
2015-11-05 16:21:28 +0000

    changed ohm to activerecord class

---
+++ app/models/vote.rb
@@ -0,0 +1,14 @@
+require 'digest/md5'
+
+class Vote < ActiveRecord::Base
+
+  validates_presence_of :ip_address
+  validates_format_of :ip_address, :with => /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\.$/
+  validates_inclusion_of :property, in: ["addiction", "harm"], error: "Vote must be for a valid property"
+  before_save :hash_vote
+
+  def hash_vote
+    hashed = Digest::MD5.hexdigest(ip_address + DateTime.strftime("%Y-%m-%d") + property + drug_id)
+  end
+
+end
