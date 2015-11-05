56e3517724932bd5249efc446658c6bc67ab3e9b
Ray <ray@thelondonvandal.com>
2015-11-05 16:22:23 +0000

    updated controller specs

--- spec/controllers/cannabinoids_controller.rb
+++ spec/controllers/cannabinoids_controller.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe CannabinoidsController, :type => :controller do
 
+  it "subclasses drugscontroller" do
+    expect(CannabinoidsController.superclass).to eq(DrugsController)
+  end
+
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 describe HomeController, :type => :controller do
 
+  describe "#index" do
+
+    it "assigns the variables for posts, alerts and latest drugs" do
+      get :index
+      expect(assigns(:recent)).to eq(Drug.last(5))
+      expect(assigns(:posts)).to eq(Post.last(5))
+      expect(assigns(:alerts)).to eq(Alert.last(5))
+    end
+
+  end
+
 end
