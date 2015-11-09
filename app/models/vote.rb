a1dfdf8b4da5f58ad6664f6ccffef3727e1a0cb1
Ray <ray@thelondonvandal.com>
2015-11-09 15:43:09 +0000

    fixed datestamp issue and removed IP validation temporarily

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,12 +3,12 @@ require 'digest/md5'
 class Vote < ActiveRecord::Base
 
   validates_presence_of :ip_address
-  validates_format_of :ip_address, :with => /\A\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\.\z/
+#  validates_format_of :ip_address, :with => /\A\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\.\z/
   validates_inclusion_of :property, in: ["addiction", "harm"], error: "Vote must be for a valid property"
   before_save :hash_vote
 
   def hash_vote
-    hashed = Digest::MD5.hexdigest(ip_address + DateTime.strftime("%Y-%m-%d") + property + drug_id)
+    hashed = Digest::MD5.hexdigest(ip_address + DateTime.now.strftime("%Y-%m-%d") + property + drug_id.to_s)
   end
 
 end
