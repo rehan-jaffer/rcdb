3b30886fe7799985abbf73498b9eb2f614ae62a6
Ray <ray@thelondonvandal.com>
2015-11-09 15:43:25 +0000

    new additions to drug form, gradually fleshing out class

--- app/forms/drug_form.rb
+++ app/forms/drug_form.rb
@@ -7,6 +7,8 @@ class DrugForm
   attribute :start_date, DateTime
   attribute :effects, Array[String]
   attribute :side_effects, Array[String]
+  attribute :affinity, Array[String]
+  attribute :legalities, Hash[Symbol => String]
   attribute :trade_names, Array[String]
   attribute :full_name, String
   attribute :description
@@ -16,5 +18,64 @@ class DrugForm
   attribute :addiction_votes, Integer
   attribute :paper_feed, String
   attribute :report_feed_url, String
+
+  def initialize(drug_params)
+    @drug_id = drug_params[:id]
+    # this should throw an error if there is no drug
+    if @drug_id
+      @drug = Drug.find(@drug_id)
+    end
+  end
+
+  def legalities
+    (@drug_id) ? @legalities ||= Locality.where(drug_id: @drug_id) : []
+  end
+
+  def effects
+    (@drug_id) ? @effects ||= Effect.where(drug_id: @drug_id, intended: true) : []
+  end
+
+  def effects=(effect_list)
+    @effects = effect_list
+  end
+
+  def side_effects
+    (@drug_id) ? @side_effects ||= Effect.where(drug_id: @drug_id, intended: false) : []
+  end
+
+  def side_effects=(effect_list)
+    @side_effects = effect_list
+  end
+
+  def method_missing(method_name, *args)
+    @drug.send(method_name, args)
+  end
+
+  def save
+ 
+    if valid?
+      persist
+      true
+    else
+      false
+    end
+
+  end
+
+  def persist
+
+    @effects.each do |effect|
+
+    end  
+
+    @side_effects.each do |side_effect|
+
+    end
+
+    @legalities.each do |legality|
+
+    end
+
+  end
 
 end
