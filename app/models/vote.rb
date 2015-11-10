300f782a76e9532511dc5508f84c1b745364775e
Ray <ray@thelondonvandal.com>
2015-11-10 14:39:48 +0000

    don't allow multiple occurences of vote with same hash (should collide once per IP per property per drug per day)

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,6 +5,7 @@ class Vote < ActiveRecord::Base
   validates_presence_of :ip_address
 #  validates_format_of :ip_address, :with => /\A\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\.\z/
   validates_inclusion_of :property, in: ["addiction", "harm"], error: "Vote must be for a valid property"
+  validates_uniqueness_of :hashed
   before_save :hash_vote
 
   def hash_vote
