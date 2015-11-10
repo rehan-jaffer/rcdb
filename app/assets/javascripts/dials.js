fefd3660254041f4dfc552ddb6f7f8809d6548e8
Ray <ray@thelondonvandal.com>
2015-11-10 09:10:58 +0000

    cleaned up ajax function for dial a little

--- app/assets/javascripts/dials.js
+++ app/assets/javascripts/dials.js
@@ -1,10 +1,31 @@
-$(document).ready(function() {
-  $(".vote-button").on("click", function() {
-    data_element = $("#"+$(this).data("target")+"_rating");
-    $.post("/votes", {vote: {drug_id: $(this).data("drug-id"), quantity: data_element.val(), property: $(this).data("target")}}, function(evt) {
-      // success code goes here
-      console.log(evt.responseText);
+var dial = {
+  trigger: $(".vote-button"),
+  access_point: "/votes",
+  setup: function() {
+    alert("setup fired!");
+    return this.trigger.on("click", function() {
+      data_element = $("#"+$(this).data("target")+"_rating");
+      alert("clicked!");
+      return $.ajax({
+        url: "/votes",
+        type: "POST",
+        dataType: "JSON",
+        data: {vote: 
+                {drug_id: $(this).data("drug-id"),
+                 quantity: data_element.val(),
+                 property: $(this).data("target")}}
+      }).success(function() {
+        // replace ratings box with a notice here
+        alert("success!");
+      }).fail(function() {
+        // display the error in this case in a popup of some kind, growl-style notification?
+        alert("failed!");
+      })
     });
-  });
+    }
+};
+
+$(document).ready(function() {
+    xhr = dial.setup();
 });
 
