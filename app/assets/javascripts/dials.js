79a28d604ae27726d31c3684d077ab7542463d87
Ray <ray@thelondonvandal.com>
2015-11-11 10:56:35 +0000

    file should use templates for voting success/failure

--- app/assets/javascripts/dials.js
+++ app/assets/javascripts/dials.js
@@ -5,6 +5,10 @@ var dial = {
     return this.trigger.on("click", function() {
       rating_property = $(this).data("target");
       data_element = $("#" + rating_property + "_rating");
+      success_source = $("#voted-container").html();
+      success_template = Handlebars.compile(success_source);
+      failure_source = $("#failed-container").html();
+      failure_template = Handlebars.compile(failure_source);
       return $.ajax({
         url: "/votes",
         type: "POST",
@@ -15,11 +19,10 @@ var dial = {
                  property: $(this).data("target")}}
       }).success(function() {
         // Use a JS template library here to replace the existing HTML with something worthwhile
-        $("#" + rating_property).html("Your vote has been submitted, votes are tallied every 24 hours.");
-//        alert("success!");
+        $("#" + rating_property).html(success_template);
       }).fail(function() {
         // display the error in this case in a popup of some kind, growl-style notification?
-        alert("failed!");
+        $("#" + rating_property).html(failure_template);
       })
     });
     }
