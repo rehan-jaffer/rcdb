3aa6302a7081eae4aa8f28cc5a30bf3e0d9835ec
Ray <ray@thelondonvandal.com>
2015-11-11 10:57:16 +0000

    run hashing method prior to validation, not before saving (will fail validation due to not being unique)

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,11 +5,11 @@ class Vote < ActiveRecord::Base
   validates_presence_of :ip_address
 #  validates_format_of :ip_address, :with => /\A\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\.\z/
   validates_inclusion_of :property, in: ["addiction", "harm"], error: "Vote must be for a valid property"
-  validates_uniqueness_of :hashed
-  before_save :hash_vote
+  validates_uniqueness_of :hashed, message: "You may only vote once per day"
+  before_validation :hash_vote
 
   def hash_vote
-    hashed = Digest::MD5.hexdigest(ip_address + DateTime.now.strftime("%Y-%m-%d") + property + drug_id.to_s)
+    self.hashed = Digest::MD5.hexdigest(ip_address + DateTime.now.strftime("%Y-%m-%d") + property + drug_id.to_s)
   end
 
 end
