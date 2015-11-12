cb508af2740d6853921a39ccce674fd3de600217
Ray <ray@thelondonvandal.com>
2015-11-12 06:20:39 +0000

    added adequate basic tests (does not test negative conditions)

--- spec/requests/api/v1/drugs_spec.rb
+++ spec/requests/api/v1/drugs_spec.rb
@@ -5,13 +5,13 @@ describe "Drugs API", :type => :request do
   describe "single drug request" do
 
     it "serves a single request successfully" do
-      get '/api/v1/drugs/Generic%20Cannabinoid'
+      get '/api/v1/drugs/Generic%20Cannabinoid.json'
       expect(response).to be_success
     end
 
     it "returns a JSON object" do
-      get '/api/v1/drugs/Generic%20Cannabinoid'
-      drug_object = JSON.parse(JSON.parse(response.body))
+      get '/api/v1/drugs/Generic%20Cannabinoid.json'
+      drug_object = JSON.parse(response.body)
       expect(drug_object.class).to be Hash 
     end
 
