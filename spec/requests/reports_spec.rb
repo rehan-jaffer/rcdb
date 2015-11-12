9637294c389d73d6d19fe8090ff90afc607698ee
Ray <ray@thelondonvandal.com>
2015-11-12 06:22:18 +0000

    added basic test for condition of no reports

--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -7,4 +7,14 @@ describe "Reports process", :type => :feature do
     expect(page.body).to have_content "No reports have been submitted yet, be the first!"
   end
 
+  describe "Javascript-based actions", js: true do
+
+    it "displays a modal if I click the 'show reports' button" do
+      visit "/cannabinoids/Generic%20Cannabinoid"
+      click_button "Submit Report"
+      expect(page).to have_selector("#submit-report-modal", visible: true)
+    end
+
+  end
+
 end
