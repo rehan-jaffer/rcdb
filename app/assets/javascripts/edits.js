4869a919c4aadf29ce2e3038dd36d4da37cd043e
Ray <ray@thelondonvandal.com>
2015-11-12 06:03:41 +0000

    don't make text disappear each time you click it

--- app/assets/javascripts/edits.js
+++ app/assets/javascripts/edits.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function() {
-  $(".drug-description").on("click", function() {
-    $(this).hide();
-    $(".drug-description-edit").show();
+//  $(".drug-description-edit-button").on("click", function() {
+//    $(".drug-description").hide();
+//    $(".drug-description-edit").show();
     // this won't work, as the text is post-processing? Replace it once you've figured things out
-    $(".drug-description-edit").val($(".drug-description").text());
-  })
+//    $(".drug-description-edit").val($(".drug-description").text());
+//  })
 });
