284946817606e02949fa92cfb61d2e48cc4702b5
Ray <ray@thelondonvandal.com>
2015-11-12 06:19:06 +0000

    added very very early implementation of JSON-based API

--- app/controllers/api/v1/drugs_controller.rb
+++ app/controllers/api/v1/drugs_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::DrugsController < ApplicationController
 
   def show
-
+    @drug = Drug.where(primary_name: params[:id]).first
+    respond_to do |format|
+      format.json {
+        render :json => @drug.as_json, status: 200
+      }
+    end
   end
 
   def index
