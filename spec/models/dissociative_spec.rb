beec34fcc68282d85ba40fe1519d6855388075a6
Ray <ray@thelondonvandal.com>
2015-11-12 06:18:16 +0000

    this is tested by the metaspec

--- spec/models/dissociative_spec.rb
+++ spec/models/dissociative_spec.rb
@@ -2,19 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Dissociative, type: :model do
 
-  before(:all) do
-    dissociative = Dissociative.new
-    dissociative.primary_name = "Ketamine"
-    dissociative.save
-  end
-
-  describe "drug class basic behaviour" do
-
-    it "parameterizes itself appropriately" do
-      dissociative = Dissociative.first
-      expect(dissociative.to_param).to eq("Ketamine")
-    end
-
-  end
-
 end
