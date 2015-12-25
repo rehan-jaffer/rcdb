590f87042933ac4fc493c2d521624231627f428b
R J <ray@thelondonvandal.com>
2015-12-25 12:15:45 +0000

    added testing for associations to drug spec

--- spec/models/drug_spec.rb
+++ spec/models/drug_spec.rb
@@ -3,22 +3,29 @@ require 'drug_name'
 
 RSpec.describe Drug, type: :model do
 
-  it "is capable of parsing its content for citation links, and enumerating them as HTML via #to_html" do
+  describe "Drug behaviour" do
+
+    let(:drug) { Drug.new }
+
+    it "is capable of parsing its content for citation links, and enumerating them as HTML via #to_html" do
     
-    @drug = Drug.new
-    @drug.primary_name = "Sample Drug"
-    @drug.description = "This is a test [http://www.test.com/]"
-    expect(@drug.to_html).to eq "This is a test <a href='#citation-1'>[1]</a><h4 class='citation-header'>+ Citations</h4>\r\n<ul class='citation-listing'>\r\n\r\n<li>1. <a href='http://www.test.com/' id='citation-1'>http://www.test.com/</a></li>\r\n</ul>\r\n"
+      drug.primary_name = "Sample Drug"
+      drug.description = "This is a test [http://www.test.com/]"
+      expect(drug.to_html).to eq "This is a test <a href='#citation-1'>[1]</a><h4 class='citation-header'>+ Citations</h4>\r\n<ul class='citation-listing'>\r\n\r\n<li>1. <a href='http://www.test.com/' id='citation-1'>http://www.test.com/</a></li>\r\n</ul>\r\n"
+
+    end
 
   end
 
-  it "returns an array of effects (desired)" do
+  describe "Drug associations" do
 
-    @drug = Drug.new
-    @drug.primary_name = "Generic Drug"
-    @drug.save
-    expect(@drug.effects.class).to be Effect::ActiveRecord_Associations_CollectionProxy
-    expect(@drug.side_effects.class).to be Effect::ActiveRecord_Associations_CollectionProxy
+      let(:has_many_associations) { [:effects, :side_effects, :affinities, :metabolites, :localities, :resources, :papers, :reports] }
+
+      it "tests the required has_many associations within the drug model" do
+        has_many_associations.each do |association|
+          expect(Drug.reflect_on_association(association).macro).to eq :has_many
+        end
+      end
 
   end
 
