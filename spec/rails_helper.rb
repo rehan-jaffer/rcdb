096107ca5a48dfaf48bff21f8c8593cdd2769710
R J <ray@thelondonvandal.com>
2015-12-25 11:07:54 +0000

    only run test data creation once

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,7 @@ end
 
 RSpec.configure do |config|
 
-  config.use_transactional_fixtures = true
+#  config.use_transactional_fixtures = true
 
   config.infer_spec_type_from_file_location!
 
@@ -30,7 +30,7 @@ RSpec.configure do |config|
 
   config.include Devise::TestHelpers, type: :controller
 
-  config.before(:all) do
+  config.before(:suite) do
 
     DatabaseCleaner.clean
 
