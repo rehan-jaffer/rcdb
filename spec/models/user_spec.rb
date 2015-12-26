eba1a8a6f51c544e00c05f38969b0391d16b6cb1
Ray <ray@thelondonvandal.com>
2015-12-26 11:31:24 +0000

    added basic tests for user class

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 describe User, type: "model" do
 
+  describe "Class Behaviour" do
+
+    let(:user) { User.new }
+
+    it "has a default class of consumer" do
+      expect(user.user_class).to eq "Consumer"
+    end
+
+    it "can be upgraded to a moderator class" do
+      user.make_user_moderator!
+      expect(user.user_class).to eq "Moderator"
+    end
+
+    it "can be upgraded to an admin" do
+      user.make_user_admin!
+      expect(user.user_class).to eq "Admin"
+    end
+
+  end
+
 end
