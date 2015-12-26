764cab81c56c271a4a81a578cc76a92fbca8ac38
R J <ray@thelondonvandal.com>
2015-12-26 11:56:56 +0000

    updated user and affinity models to pass tests

--- app/models/affinity.rb
+++ app/models/affinity.rb
@@ -1,2 +1,11 @@
 class Affinity < ActiveRecord::Base
+
+  def to_s
+    pretty_print
+  end
+
+  def pretty_print
+    "#{value}#{order}"
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def user_class
+    user_classes = ["Consumer","Moderator","Admin"]
+    user_classes.each do |user_class|
+        return user_class if has_role? user_class.downcase.to_sym
+    end
+    return "Consumer"
+  end
+
+  def make_user_moderator!
+    add_role :moderator
+  end
+
+  def make_user_admin!
+    add_role :admin
+  end
+
 end
